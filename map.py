a5e4f222b25fed0e1a61c91ddee2e399ff027e9b
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2017-02-15 19:44:30 -0600

    Fully working system with "extra work"

--- map.py
+++ map.py
@@ -10,19 +10,27 @@ for line in sys.stdin:
         continue
 
     if attribute == "text":
-        newline = ast.literal_eval(line)["text"]
-        newline = newline.strip()
-        words = newline.split()
-        for word in words:
-            if word == keyword:
-                print '%s\t%s' % (word, 1)
+        try:
+            newline = ast.literal_eval(line)["text"]
+            newline = newline.strip()
+            words = newline.split()
+            for word in words:
+                if word == keyword:
+                    print '%s\t%s' % (word, 1)
+        except:
+            continue
+
 
     elif attribute == "hashtags":
-        hts = ast.literal_eval(line)["entities"]["hashtags"]
-        if not hts == []:
-            for ht in hts:
-                if ht["text"] == keyword:
-                    print '%s\t%s' % (ht["text"], 1)
+        try:
+            hts = ast.literal_eval(line)["entities"]["hashtags"]
+            if not hts == []:
+                for ht in hts:
+                    if ht["text"] == keyword:
+                        print '%s\t%s' % (ht["text"], 1)
+        except:
+            continue
+
 
 
 
